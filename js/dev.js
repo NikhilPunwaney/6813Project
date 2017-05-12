fa60c8fa9c6083bae577052a3e920e0f9d77316a
Eliza Reilly <reillye@mit.edu>
2017-05-11 22:21:54 -0400

    fixed parent interface

--- js/dev.js
+++ js/dev.js
@@ -260,7 +260,120 @@ var Setup = function () {
             elem.removeChild(header1);
     };
   }
-  
+  this.addNewsFeedParent = function(date, title, desc) {
+    localStorage.setItem("edit", "off");
+    var elem = document.getElementById('feed');
+
+    var header1 = document.createElement("H1");       
+    var t1 = document.createTextNode(date);  
+    header1.style.fontSize = "12pt";
+    header1.style.marginTop = "30px";
+    header1.style.color = "white";
+    header1.style.fontWeight = "bold";
+    header1.style.fontFamily =  "'Quicksand', sans-serif";
+    header1.appendChild(t1); 
+
+
+    var header2 = document.createElement("H1");       
+    var t2 = document.createTextNode(title);
+    header2.style.fontSize = "24pt";
+    header2.style.marginTop = "10px";
+    header2.style.color = "black";
+    header2.style.fontFamily =  "'Gloria Hallelujah', sans-serif";       
+    header2.appendChild(t2); 
+
+
+    var para = document.createElement("P"); 
+    var t3 = document.createTextNode(desc);   
+    para.style.fontSize = "12pt";
+    para.style.marginTop = "10px";
+    para.style.color = "white";
+    para.style.fontFamily =  "'Quicksand', sans-serif";  
+    para.align = "left";
+    para.appendChild(t3);
+
+    var line = document.createElement("P"); 
+    var t4 = document.createTextNode("---------");   
+    line.style.fontSize = "12pt";
+    line.style.marginTop = "10px";
+    line.style.color = "white";
+    line.style.fontFamily =  "'Quicksand', sans-serif"; 
+    line.id = "line" ;     
+    line.appendChild(t4);
+
+    var btn2 = document.createElement("BUTTON");
+    var t2 = document.createTextNode("edit"); 
+    btn2.appendChild(t2); 
+    btn2.style.width = "90px";
+    btn2.style.marginTop = "20px";
+    btn2.style.marginLeft = "10px";
+    btn2.style.marginRight = "10px";
+    btn2.style.padding = "10px 10px 10px 10px";
+    btn2.style.borderRadius = "12px";
+    btn2.style.backgroundColor = "white";
+    btn2.style.border = "white";
+    btn2.style.fontFamily = "Quicksand, sans-serif"; 
+
+    var btn = document.createElement("BUTTON");
+    var t = document.createTextNode("delete"); 
+    btn.appendChild(t);  
+    btn.style.width = "90px";
+    btn.style.marginTop = "20px";
+    btn.style.marginLeft = "10px";
+    btn.style.marginRight = "10px";
+    btn.style.padding = "10px 10px 10px 10px";
+    btn.style.borderRadius = "12px";
+    btn.style.backgroundColor = "white";
+    btn.style.border = "white";
+    btn.style.fontFamily = "Quicksand, sans-serif"; 
+
+    var space = document.createElement("P"); 
+
+
+    elem.insertBefore(space, elem.firstChild);
+    elem.insertBefore(line, elem.firstChild);
+    elem.insertBefore(para, elem.firstChild);
+    elem.insertBefore(header2, elem.firstChild);
+    elem.insertBefore(header1, elem.firstChild);
+
+    btn.onclick = function() {
+        if(confirm("Are you sure you want to delete your post?")){
+            elem.removeChild(space);
+            elem.removeChild(btn);
+            elem.removeChild(btn2);
+            elem.removeChild(line);
+            elem.removeChild(para);
+            elem.removeChild(header2);
+            elem.removeChild(header1);
+        }
+
+    }; 
+
+        btn2.onclick = function() {
+        
+
+            if (typeof(Storage) !== "undefined") {
+                 // Code for localStorage/sessionStorage.
+                localStorage.setItem("edit", "on");
+                 localStorage.setItem("date", date);
+                 localStorage.setItem("title", title);
+                 localStorage.setItem("description", desc);
+                 console.log(typeof(Storage));
+            } else {
+                 // Sorry! No Web Storage support..
+                 console.log("undefined");
+                }
+           // dev.visitPage("./teacher_Newpost.html");
+            window.location = "./teacher_Newpost.html";
+             elem.removeChild(space);
+            elem.removeChild(btn);
+            elem.removeChild(btn2);
+            elem.removeChild(line);
+            elem.removeChild(para);
+            elem.removeChild(header2);
+            elem.removeChild(header1);
+    };
+  }
 }
 
 
